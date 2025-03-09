27de26e1ad480bb49e255ae54a997a013e404e8c
Laurent Chardon <laurent.chardon@gmail.com>
2025-03-08 20:47:01 -0500

    update_jails.sh: create jails if missing; improve arch handling

--- update_jails.sh
+++ update_jails.sh
@@ -7,28 +7,66 @@ handle_sigint() {
 }
 trap handle_sigint INT
 
+extract_version() {
+    local jail="$1"
+    local base_jail suffix version major minor release
+    # Remove architecture suffix (text after the first hyphen)
+    base_jail="${jail%%-*}"
+    
+    case "$base_jail" in
+        *cur)
+            suffix="CURRENT"
+            version="${base_jail%cur}"
+            ;;
+        *rel)
+            suffix="RELEASE"
+            version="${base_jail%rel}"
+            ;;
+        *)
+            echo "Unrecognized jail naming: $jail" >&2
+            return 1
+            ;;
+    esac
+
+    # Extract major (all but the last digit) and minor (last digit)
+    major="${version%?}"
+    minor="${version##$major}"
+    release="${major}.${minor}-${suffix}"
+    echo "$release"
+}
+
+jail_list="$(poudriere jail -l)"
 JAILS="$(cat jails)"
 
 for jail in $JAILS
 do
-	sudo poudriere jail -k -j $jail
-	if echo $jail | grep -q cur
-	then
-		major="${jail%?cur*}"
-		minor="${jail#??}"
-		minor="${minor%%cur*}"
-		release="${major}.${minor}-CURRENT"
-    if [ "$variable" == *i386 ]
-    then 
-      JAILARCH='- a i386'
-    else
-      JAILARCH=''
+    echo
+    echo ===== Updating jail $jail
+    sudo poudriere jail -k -j "$jail"
+    
+    release="$(extract_version "$jail")"
+    if [ $? -ne 0 ]; then
+        echo "Skipping jail $jail due to naming issue."
+        continue
     fi
-		if poudriere jail -l | grep -qw "$jail"; then
-			sudo poudriere jail -d -j $jail -y
-		fi
-		sudo poudriere jail -c $JAILARCH -j $jail -v $release
-	else
-		sudo poudriere jail -u -j $jail
-	fi
+    arch="${jail##*-}"
+    arch="$(echo "$arch" | sed 's/_/./g')"
+    echo '*****' $arch
+
+    if echo "$jail" | grep -q cur; then
+        # CURRENT jail: if it exists, delete it, then re-create.
+        if echo $jail_list | grep -qw "$jail"; then
+            sudo poudriere jail -d -j "$jail" -y
+        fi
+        sudo poudriere jail -c -a $arch -j "$jail" -v "$release"
+    else
+        # RELEASE jail: create if it doesn't exist, else update.
+        if ! echo $jail_list | grep -qw "$jail"; then
+	    echo === Creating jail $jail release $release
+            sudo poudriere jail -c -a $arch -j "$jail" -v "$release"
+        else
+            sudo poudriere jail -u -j "$jail"
+        fi
+    fi
 done
+
