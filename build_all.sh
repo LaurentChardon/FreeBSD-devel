dbb0b9d9d7ec23b944c9ba66bccd997cc4fb4dd2
Laurent Chardon <laurent.chardon@gmail.com>
2025-11-11 06:25:57 -0500

    build_all.sh: clean only the packages specified

--- build_all.sh
+++ build_all.sh
@@ -5,5 +5,5 @@ PORTS_FILE=${1:-./my_ports}
 
 for jail in $JAILS
 do
-	sudo script -q build_all-$jail.log nice -n 10 poudriere -A bulk -j $jail -b latest -f $PORTS_FILE 2>&1 
+	sudo script -q build_all-$jail.log nice -n 10 poudriere -A bulk -j $jail -C -b latest -f $PORTS_FILE 2>&1 
 done
